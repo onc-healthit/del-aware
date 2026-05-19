8edce4c3d4245456b73a4e7e747ea33e69be0d4d
aholmes@mitre.org <14923551+holmesie@users.noreply.github.com>
2026-05-19 11:11:47 -0400

    fix url ref

--- lib/delaware/models/profile.rb
+++ lib/delaware/models/profile.rb
@@ -51,9 +51,7 @@ module Delaware
         if url.include?('%7C')
           version = url.split('%7C').last
           version = version.gsub(/.0$/, '') if version.ends_with?('.0')
-          url.gsub('/StructureDefinition/', "/STU#{version}/StructureDefinition-").gsub(/%7C.*/, ending)
-        else
-          url
+          url = url.gsub('/StructureDefinition/', "/STU#{version}/StructureDefinition-").gsub(/%7C.*/, ending)
         end
         url.gsub('http://', 'https://')
       end
